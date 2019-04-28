92c97ae4e8e29302d774a7a72ad60f9bf4e7c4aa
Paul Whurr <pawhurr@gmail.com>
2019-04-27 22:15:12 -0400

    added win condition, fixed bug for showing answer, fixed bug for guesses resetting

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var Word = require("./word");
 var inquirer = require("inquirer");
-var guesses = 10;
+var guesses = 7;
 var wordChoices = ["BART", "HOMER", "MAGGIE"];
 var word = new Word();
 var correct = 0;
+var falseCount = 0;
 
 
 function createWord() {
-    
+    word.wordBuild = [];
+    guesses = 7;
     var randNum = Math.floor(Math.random() * (wordChoices.length));
     var computerChoice = wordChoices[randNum];
 
@@ -27,10 +29,11 @@ function start() {
         }
     ]).then(function(answer) {
         if (answer.confirm === true) {
+            console.log("\nGood choice!\n");
             createWord();
             play();
         } else {
-            console.log("Bye!")
+            console.log("Well, goodbye then. Maybe next time.")
         }
     })
 };
@@ -40,6 +43,7 @@ start();
 function play() {
     if (guesses > 0) {
         correct = 0;
+        falseCount = 0;
         console.log("\nGuesses left: " + guesses);
         console.log("~~~~~~~~~~~~\n");
         word.wordView();
@@ -55,7 +59,10 @@ function play() {
             for (var i = 0; i < word.wordBuild.length; i++) {
                 if (answer.userInput.toUpperCase() === word.wordBuild[i].char) {
                     correct++;
-                }
+                };
+                if (word.wordBuild[i].beenGuessed === false) {
+                    falseCount++;
+                };
             }
             if (correct > 0) {
                 console.log("\nCorrect!\n");
@@ -63,13 +70,24 @@ function play() {
                 console.log("\nIncorrect!\n");
                 guesses--;
             };
+            if (falseCount === 0) {
+                word.wordView();
+                console.log("\nCongratulations! You're a winner!\n\nNow that you're on a winning streak I have one question for you...\n");
+                start();
+                return;
+            }
            
             play();
             
         })
     } else {
-        console.log("The answer was: " + word.wordBuild.split(",").join(""));
-        console.log("\nGame Over");
+        for (var i = 0; i < word.wordBuild.length; i++) {
+            word.wordBuild[i].beenGuessed = true;
+        }
+        console.log("The answer was: \n");
+        word.wordView();
+        console.log("\nGame Over. Too bad. You can't go out like that. So, I have a question for you...\n");
+        start();
     }
 }
 
